22d8b24e187fa1b153e4a5eaa62b72dc255bca59
parthbavikaritsit-4050 <parthbavikar.it.sit@gmail.com>
2026-05-23 17:50:18 +0530

    practice: learned JavaScript conditionals

---
+++ javascript/conditioals/nested.js
@@ -0,0 +1,18 @@
+let a=10;
+let c=110;
+let b=150;
+
+if(a>b){
+    if(a>c)console.log("A is Largest");
+
+    else console.log("C is Largest");
+    
+}
+
+
+if(b>a){
+    
+    if(b>c) console.log("b is Largest");
+}
+
+else console.log("c is Largest");
